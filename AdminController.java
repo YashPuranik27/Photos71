090cf4e2a202aa33c0fd20261fa0b4bca7ee7e9a
Yash Puranik <yap13@scarletmail.rutgers.edu>
2023-11-16 23:04:06 +0000

    AdminController.java edited. We still need to add method to delete a user and add a user in the admin panel.

--- AdminController.java
+++ AdminController.java
@@ -6,15 +6,32 @@ import javafx.fxml.FXML;
 public class AdminController implements LogoutController {
 
     @FXML
-    ListView<String> UserList;
+    ListView<String> UserList; // Listview of users
     @FXML
-    Button AddUserButton;
+    Button AddUserButton; // Button to add a user
     @FXML
-    Button DelUserButton;
+    Button DelUserButton; // Button to delete a user
     @FXML
-    Button LogOffButton;
+    Button LogOffButton; // Button to log off
     @FXML
-    TextField Enteruser;
+    TextField Enteruser; // Text field to enter a username
 
+    ObservableList<String> observableArrayList;
+
+    public void start() { // This method is called when the scene is loaded
+        refresh();
+        if (!observableArrayList.isEmpty()) UserList.getSelectionModel().select(0);
+    }
+    
+    public void refresh() { // Refreshes the listview
+        UserList.refresh();
+        observableArrayList = FXCollections.observableArrayList(Photos.driver.admin.getUsernameList()); // Gets the list of usernames
+        UserList.setItems(observableArrayList);
+        UserList.refresh();
+    }
+
+    public void logOut(ActionEvent e) throws IOException { // Logs out the user
+        logMeOut(e);
+    }
 
 }
