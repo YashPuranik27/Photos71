f4b2cccc3b932944fd1515d341a912945fc85572
Yash Puranik <yap13@scarletmail.rutgers.edu>
2023-11-16 23:34:02 +0000

    AdminController.java edited. Added addUser, showAlert (opens window if user input is empty, is admin, or if user already exists), and deleteUser.

--- AdminController.java
+++ AdminController.java
@@ -22,7 +22,63 @@ public class AdminController implements LogoutController {
         refresh();
         if (!observableArrayList.isEmpty()) UserList.getSelectionModel().select(0);
     }
-    
+  
+    public void addUser(ActionEvent e) throws IOException {
+        String userInput = Enteruser.getText().trim();
+
+        if (userInput == null || userInput.isEmpty()) { // If user input is empty
+            showAlert("Invalid Input - ERROR", "Input field is empty. Please try again.");
+            return;
+        }
+
+        if (userInput.equals("admin")) { // There is only one admin and we already defined it in admin.java
+            showAlert("Invalid Input", "The username admin is protected. Please try a different username!");
+            return;
+        }
+
+        if (Photos.driver.admin.userExists(userInput) != -1) { // If user already exists
+            showAlert("User Already Exists - ERROR", "This Username already exists. Please enter a new username.");
+            return;
+        }
+
+        Photos.driver.admin.addUser(userInput);
+        Persistence.save(Photos.driver); // Save the changes to info.dat through Persistence.java
+        refresh();
+        UserList.getSelectionModel().select(userInput);
+        Enteruser.clear();
+    }
+
+    private void showAlert(String title, String content) { // Helper method to show alerts. opens a new alert window
+        Alert alert = new Alert(AlertType.ERROR);
+        alert.setTitle(title);
+        alert.setContentText(content);
+        alert.showAndWait();
+    }
+
+
+    public void deleteUser(ActionEvent e) throws IOException {
+        int deleteIndex = UserList.getSelectionModel().getSelectedIndex(); // Get the index of the selected item
+        if (deleteIndex < 0) return; // No selection made, exit early
+
+        String selectedItem = UserList.getSelectionModel().getSelectedItem(); // Get the selected item
+        Alert alert = new Alert(AlertType.CONFIRMATION, "Confirm you want to delete " + selectedItem + ".", ButtonType.OK, ButtonType.CANCEL);
+        alert.setTitle("Confirm Deletion");
+
+        ButtonType result = alert.showAndWait().orElse(ButtonType.CANCEL); // we use orElse to provide a default value
+
+        if (result == ButtonType.OK) {
+            Photos.driver.admin.deleteUser(selectedItem);
+            refresh();
+            Persistence.save(Photos.driver);
+
+            // this is to adjust the selection in the list
+            int usersSize = Photos.driver.admin.getUsers().size();
+            if (usersSize > 0) {
+                UserList.getSelectionModel().select(Math.max(0, Math.min(deleteIndex, usersSize - 1)));
+            }
+        }
+    }
+  
     public void refresh() { // Refreshes the listview
         UserList.refresh();
         observableArrayList = FXCollections.observableArrayList(Photos.driver.admin.getUsernameList()); // Gets the list of usernames
