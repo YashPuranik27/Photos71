3fefb4983e437acc6035f7278107b17bae47a2f5
Yash Puranik <yap13@scarletmail.rutgers.edu>
2023-11-16 22:44:59 +0000

    Added methods that pertain to the writeup. I do not think we need to add any more.

--- Admin.java
+++ Admin.java
@@ -10,5 +10,37 @@ public class Admin implements Serializable {
     private static final String DEFAULT_ADMIN_USERNAME = "admin";
     private List<User> users;
     private String username;
-    
+
+    public Admin(List<User> users) {
+        this.users = users;
+        this.username = DEFAULT_ADMIN_USERNAME;
     }
+
+    public Admin() {
+        this(new ArrayList<>());
+    }
+
+    public void setUsers(List<User> users) {
+        this.users = users;
+    }
+
+    public void addUser(String userName) {
+        users.add(new User(userName));
+    }
+
+    public List<User> getUsers() {
+        return new ArrayList<>(users); // This should return a copy to avoid external modification
+    }
+
+    public List<String> getUsernameList() {
+        return users.stream().map(User::getName).collect(Collectors.toList());
+    }
+
+    public int userExists(String userName) {
+        return users.stream().map(User::getName).collect(Collectors.toList()).indexOf(userName);
+    }
+
+    public void deleteUser(String userName) {
+        users.removeIf(user -> user.getName().equals(userName));
+    }
+}
